0ea88fb411d146648a4f658e27e32eb5c251562c
stingfu <stingfupvtltd@gmail.com>
2025-07-23 15:09:13 +0530

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -57,7 +57,7 @@ const Header: React.FC = () => {
 
   const settingsOptions = [
     { name: 'Account Settings', path: '/account-settings', icon: <Settings className="w-4 h-4" /> },
-    { name: 'Profile', path: '/profile', icon: <User className="w-4 h-4" /> },
+    { name: 'Profile setting', path: '/profile setting', icon: <User className="w-4 h-4" /> },
  ];
 
   const handleDropdownClick = (dropdownName: string) => {
